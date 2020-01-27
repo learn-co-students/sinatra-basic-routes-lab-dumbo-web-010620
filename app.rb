51eb1fb3a839ca6bbd75c9524fece4f83e2c810c
Tcaprioli <Capriolityler@gmail.com>
2020-01-27 17:41:02 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do 
+        "Hello, World!"
+      end
+
+      get '/name' do
+        "My name is Tyler"
+      end
+
+      get '/hometown' do
+        "My hometown is ny"
+      end
+
+      get '/favorite-song' do 
+        "My favorite song is shea butter baby"
+      end
 end
